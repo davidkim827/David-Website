131c770a045899a583cc4161bd035b97d42d7737
davidkim827 <davidkim827@gmail.com>
2020-12-05 13:51:44 -0500

    changeup and add app component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,52 +1,101 @@
+<script>
+	import Skills from "./Skills.svelte";
+	import NavBar from "./NavBar.svelte";
+
+	const name = "DAVID KIM";
+	const job = "Software Engineer";
+	const yearsExperience = 1.5;
+</script>
+
 <style>
-	main {
+	#intro {
+		margin-left: auto;
+		margin-right: auto;
+		align-content: center;
+		width: 500px;
+	}
+
+	#title {
+		margin-left: auto;
+		margin-right: auto;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 500px;
+	}
+	#contact-info {
+		margin: auto;
+		padding-left: 15px;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.contact-images {
+		width: 30px;
+		height: 30px;
+	}
+	.contact-images:hover {
+		transform: scale(1.5);
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	#heading {
+		text-align: center;
+		width: 500px;
+	}
+	#short-intro{
+		margin-top: 30px;
+		margin-bottom: 30px;
+	}
+	.name {
+		margin-bottom: 0;
+	}
+	img {
+		border-radius: 50%;
+		max-width: 50%;
+		height: auto;
+		margin-left: auto;
+		margin-right: auto;
+		width: 500px;
 	}
 </style>
 
+<div id="intro">
+	<div id="title">
+		<img
+			src="./img/me_halloween_2014.jpg"
+			alt="Me on Halloween 2014"
+			width="250"
+			height="250" />
+	</div>
 
-<script>	
-	async function fetchMessage(){
-		const message = await fetch("http://localhost:8000/test")
-		const jsn = await message.json()
+	<div id="heading">
+		<h1 class="name">{name}</h1>
+		<div id="contact-info">
+			<a href="https://www.linkedin.com/in/dkim827/">
+				<img
+					class="contact-images"
+					src="./img/linkedin.jpg"
+					alt="My LinkedIn Page" />
+			</a>
+			<a
+				href="mailto:davidkim827@gmail.com?subject=We Want to Hire YOU, David!">
+				<img
+					class="contact-images"
+					src="./img/mail icon.png"
+					alt="Email me!" />
+			</a>
+			<a href="https://github.com/davidkim827"><img
+					class="contact-images"
+					src="./img/github.png"
+					alt="My Github Link!" /></a>
+			<a
+				href="https://www.google.com/maps/place/Brooklyn,+NY/@40.6451594,-74.0850816,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24416947c2109:0x82765c7404007886!8m2!3d40.6781784!4d-73.9441579"><img
+					class="contact-images"
+					src="./img/location.jpeg"
+					alt="Brooklyn is the best borough!" /></a>
+		</div>
+		<div id="short-intro">
+			<p>{job} with {yearsExperience} years in Security</p>
+		</div>
+	</div>
+</div>
 
-		if (message.ok){
-			console.log
-			return jsn.Message
-		}
-		throw new Error(text)
-	};
+<Skills />
 
-	let name = fetchMessage()
-
-
-</script>
-
-<main>
-
-{#await name}
-	<h1>Hello World!</h1>
-{:then message}
-	<h1>Hello {message}!</h1>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
-
-<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<NavBar />
