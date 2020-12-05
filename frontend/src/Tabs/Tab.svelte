9ba0c39b4bcd5838578b1437d1ec422fbb6bce75
davidkim827 <davidkim827@gmail.com>
2020-12-05 13:51:10 -0500

    add tabs component

---
+++ frontend/src/Tabs/Tab.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+</script>
+
+<style>
+	button {
+		background: none;
+		border: inherit;
+		border-bottom: 2px solid white;
+		border-radius: 0;
+		margin: auto;
+		color: #ccc;
+		border-radius: 30px;
+	}
+
+	button:hover{
+		background-color: #24a0ed;
+        box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1);
+        color: #fff;
+	}
+	
+	.selected {
+		background-color: #24a0ed;
+		color: #fff;
+		transition:all 5s;
+	}
+</style>
+
+<button class:selected="{$selectedTab === tab}" on:click="{() => selectTab(tab)}">
+	<slot></slot>
+</button>
---
+++ frontend/src/Tabs/TabList.svelte
@@ -0,0 +1,14 @@
+<div class="tab-list">
+	<slot></slot>
+</div>
+
+<style>
+	.tab-list {
+		width: 500px;
+		display: flex;
+		justify-content: space-between;
+		margin: auto;
+		text-align: center;
+		border-radius: 20px;
+	}
+</style>
